baaab41a4a6ddc31bf25289651cfc886c91c8e7a
Michał Pierzchała <thymikee@gmail.com>
2025-04-02 18:34:27 +0200

    test

--- .github/actions/find-artifact/index.mjs
+++ .github/actions/find-artifact/index.mjs
@@ -91,6 +91,7 @@ async function run() {
     } else if (prNumber && reSign) {
       artifactName = `${name}-${prNumber}`;
     }
+    console.log({ artifactName });
     core.setOutput("artifact-name", artifactName);
     core.setOutput("artifact-id", firstArtifact.id);
     core.setOutput("artifact-url", url);
